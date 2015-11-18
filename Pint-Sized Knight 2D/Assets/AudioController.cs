7d67b80345a15cc34b13e08dbe004ffe8a36d949
Mike Dean <msd14@hampshire.edu>
2015-11-18 00:57:12 -0500

    AudioController

--- Pint-Sized Knight 2D/Assets/AudioController.cs
+++ Pint-Sized Knight 2D/Assets/AudioController.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
     public AudioClip[] sounds;
+    private AudioSource sound;
+    private VariableController variables;
 	// Use this for initialization
 	void Start () {
-	    
+        variables = GameObject.Find("Variables").GetComponent<VariableController>();
+        sound = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (variables.attack)
+            playAttackSound();
+        if (variables.defend)
+            playDefendSound();
 	}
+    public void playAttackSound()
+    {
+        sound.clip = sounds[new System.Random().Next(1, 4)];
+        if (!sound.isPlaying)
+            sound.Play();
+    }
+    public void playDefendSound()
+    {
+        sound.clip = sounds[0];
+        if (!sound.isPlaying)
+            sound.Play();
+    }
 }
