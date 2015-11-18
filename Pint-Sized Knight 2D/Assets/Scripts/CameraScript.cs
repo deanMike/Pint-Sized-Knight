d9e34a56a1d1d01466ed2a7109c35406bdeb6f34
Mike Dean <msd14@hampshire.edu>
2015-11-17 23:27:53 -0500

    2D version lots of art

---
+++ Pint-Sized Knight 2D/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pan(Camera.main.WorldToViewportPoint(player.transform.position));	}
+
+
+    public void pan(Vector3 charPos) {
+        if (charPos.x >= 1) {
+            gameObject.transform.Translate(new Vector3(12.29f, 0, 0), Space.Self);
+        }
+        if (charPos.x <= 0) {
+            gameObject.transform.Translate(new Vector3(-12.29f, 0, 0), Space.Self);
+        }
+        if (charPos.y >= 1) {
+            gameObject.transform.Translate(new Vector3(0, 12.81f, 0), Space.Self);
+        }
+        if (charPos.y <= 0) {
+            gameObject.transform.Translate(new Vector3(0, -12.81f, 0), Space.Self);
+        }
+    }
+}
---
+++ Pint-Sized Knight/Assets/Scripts/VariableController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariableController : MonoBehaviour {
+
+    public float playerHealth;      // Player's starting health.
+    public int numHitsKill;         // Number of hits required to kill the weakest monster.
+    public float playerSpeed;       // Maximum player movement speed.
+    public float monsterSpeed;      // Minimum monster movement speed.
+    public float[] volume;          // Array of volume values.
+    public float monstersOnScreen;  // Maximum number of monsters on screen.
+
+}
